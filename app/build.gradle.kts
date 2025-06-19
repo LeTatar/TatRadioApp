d82efb563d6c4bb15fdb41172958f5a1255a2e13
LeTatar <toxild@gmail.com>
2025-06-19 23:02:38 +0300

    иконки закруглены

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,4 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation ("androidx.media3:media3-exoplayer:1.7.1")
-   }
+}
